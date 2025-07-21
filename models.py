e288afb40baf32b70cf24ce7083b2ffd7c1c841d
vsangireddy27 <vsangireddy25@gmail.com>
2025-07-21 16:06:22 -0700

    added manager_id field and value

--- models.py
+++ models.py
@@ -17,7 +17,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.String(20), nullable=False) 
+    manager_id = db.Column(db.Integer, nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
